ca4c25015304bf41958d97b0e99a90f259495c7f
Bludidi <bludidi5@gmail.com>
2023-05-18 13:13:43 +0200

    Add reducer

---
+++ src/Redux/Home/carSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+import fecthCars from './fetchCars';
+
+const initialState = {
+  cars: [],
+  status: '',
+};
+
+const carsSlice = createSlice({
+  name: 'cars',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(
+      fecthCars.pending,
+      (state) => {
+        state.status = 'loading';
+      },
+      builder.addCase(fecthCars.fulfilled, (state, action) => {
+        state.status = 'ok';
+        state.cars = action.payload;
+      })
+    );
+  },
+});
+
+export default carsSlice.reducer;
