e233aab5a605b08f25f368ab83f85c3d75cb20cf
Bludidi <bludidi5@gmail.com>
2023-05-26 22:18:27 +0200

    Create a car slice

--- src/Redux/CarSlice.js
+++ src/Redux/CarSlice.js
@@ -1,36 +1,29 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { getCar } from '../redux/SingleCarSlice';
-import CarCard from '../components/CarCard';
-import Load from '../components/Load';
-
-const Car = () => {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  const car = useSelector((state) => state.singleCar.car);
-  const loading = useSelector((state) => state.singleCar.loading);
-
-  useEffect(() => {
-    dispatch(getCar(id));
-  }, [dispatch, id]);
-  if (loading) {
-    return <div className="flex justify-center min-h-[60vh] items-center"><Load /></div>;
-  }
-  return (
-    <div className="flex  justify-center items-center">
-      <div className="w-full mt-4">
-        <h1 className="text-center text-base md:text-2xl font-bold">car Details</h1>
-        <CarCard
-          id={car.id}
-          name={car.name}
-          image={car.image}
-          color={car.color}
-          description={car.description}
-          price={car.price}
-        />
-      </div>
-    </div>
-  );
+/* eslint-disable */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import Url from '../api/Urlapi';
+const initialState = {
+  loading: true,
+  data: [],
+  message: '',
+  error: null,
+  car: {},
 };
-export default Car;
+const token = localStorage.getItem('token');
+export const createCar = createAsyncThunk(
+  'Car/createCar',
+  async (car, { rejectWithValue }) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const { data } = await axios.post(`${Url}/api/v1/cars`, car, config);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
